498a7ab509496319fcef50e0184c53d47874d73d
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-08-10 22:37:27 +0800

    added home

--- routers/App.js
+++ routers/App.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/landing.html'));
 });
 
+router.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/home.html'));
+});
+
 module.exports = router;
