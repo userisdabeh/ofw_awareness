a3f43bd3738439e32e84087f83600d2368471311
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-08-10 21:16:03 +0800

    fresh code

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const router = require('./routers/App.js');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
