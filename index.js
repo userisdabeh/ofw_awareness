4a06e3f7e4797909ecb1cdab79e78e1053ed21df
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-08-10 22:28:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-require('dotenv').config();
 const router = require('./routers/App.js');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static('public'));
-app.use('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', router);
 
